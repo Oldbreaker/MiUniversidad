13205a8f1bdfc8832d77addfb5eb2af0f2461ff3
Fernando Valenzuela Ramos <amdati5000@gmail.com>
2020-12-07 23:13:55 -0600

    first commit

---
+++ Modulos/Academica/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Modulos.Academica.models import *
+
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
